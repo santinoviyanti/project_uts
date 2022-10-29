e10286a1dd362839132df0ca28302cb749e9d7d4
Santi Noviyanti <115691512+santinoviyanti@users.noreply.github.com>
2022-10-30 01:38:31 +0700

    nomer 1
    jawaban uts nomer 1

---
+++ uts_no1.py
@@ -0,0 +1,31 @@
+# 1. jelaskan type data dalam bahasa dasar pemograman dan beri contoh data:
+
+# - Type data numbers (Numerik) adalah segala jenis tipe data yang berupa angka
+tinggi = 160
+berat = 45.5
+energi = 50j
+
+print("data : ", tinggi)
+print("- bertipe : ", type(tinggi))
+print("data : ", berat)
+print("- bertipe : ", type(berat))
+print("data : ", energi)
+print("- bertipe : ", type(energi))
+
+# - Type data string adalah tipe data yang berisi sebuah text yang diapit oleh tanda petik
+Nama = "Santi Noviyanti"
+NPM = "202210715286"
+
+print("Nama : ", Nama)
+print("- bertipe : ", type(Nama))
+print("NPM : ", NPM)
+print("- bertipe : ", type(NPM))
+
+# - Type data boolean adalah tipe data yang digunakan untuk menyatakan pernyatan true/false
+prodi_informatika = True
+fakultas_psikologi = False
+
+print("apakah saya mahasiswa prodi informatika? ", prodi_informatika)
+print("- bertipe : ", type(prodi_informatika))
+print("apakah prodi informatika berada difakultas psikologi? ", fakultas_psikologi)
+print("- bertipe : ", type(fakultas_psikologi))

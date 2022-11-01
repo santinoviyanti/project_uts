2a5f3b1085ec90a1be87f3152cc2d0aa778df535
Santi Noviyanti <115691512+santinoviyanti@users.noreply.github.com>
2022-11-01 19:59:24 +0700

    nomer 2

---
+++ uts_no2.py
@@ -0,0 +1,27 @@
+# 2. Buat contoh kode program untuk membedakan type data di python
+
+# - tipe data list
+nama_makanan = ["bakso", "kwetiau goreng", "donat", "martabak"]
+print("list makanan : ", nama_makanan)
+nilai_tugas = [80, 79, 85, 90]
+print("nilai tugas data pemograman : ", nilai_tugas)
+
+# - tipe data tuple
+lagu_seventeen =('circles', 'hot', 'fear', 'clap',)
+print("lagu seventeen : ", lagu_seventeen)
+
+# - tipe data set
+grup_kpop ={'BTS', 'straykids', 'treasure', 'seventeen'}
+print("kpop boy grup : ", grup_kpop)
+
+# - tipe data dictionary
+data_dict ={
+    'key':'value',
+    'makanan':'bakso',
+    'nilai':'90',
+    'list':lagu_seventeen,
+}
+print(data_dict['key'])
+print(data_dict['makanan'])
+print(data_dict['nilai'])
+print(data_dict['list'])

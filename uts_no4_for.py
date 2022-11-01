606600971223aa0fc9cd92382a77994af4ea0564
Santi Noviyanti <115691512+santinoviyanti@users.noreply.github.com>
2022-11-01 20:01:54 +0700

    nomer 4 (for)

---
+++ uts_no4_for.py
@@ -0,0 +1,11 @@
+# 4. Buat contoh kode program untuk logika pengulangan menggunakan for dan while
+
+listMakanan = [
+  'sate ayam', 'bika ambon', 'nasi goreng', 'gado-gado', 'bakso',
+  'rendang', 'gulai ikan', 'soto', 'nasi uduk', 'kerak telor',
+  'sop buntut', 'ayam goreng', 'gudeg', 'bakmi', 'rawon',
+  'rujak cingur', 'pecel lele', 'opor ayam', 'bubur ayam', 'asinan'
+]
+
+for Makanan in listMakanan:
+  print(Makanan)

0cc6d2335c93351972c8810c9861e87f67007c49
Santi Noviyanti <115691512+santinoviyanti@users.noreply.github.com>
2022-11-01 20:02:49 +0700

    nomer 4 (while)

---
+++ uts_no4_while.py
@@ -0,0 +1,9 @@
+# 4. Buat contoh kode program untuk logika pengulangan menggunakan for dan while
+
+angka = 0
+
+while angka < 5:
+    print('project uts SANTI NOVIYANTI 202210715286')
+    angka = angka + 1
+
+print('project uts dasar pemograman telah selesai dikerjakan')
